24d1813e927575b618f27219a497dc342acbd6ea
tobe <583590744@qq.com>
2025-02-16 22:33:05 +0800

    增加prettier tailwind

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  tabWidth: 2,
+  printWidth: 200,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: false,
+  arrowParens: 'avoid'
+};

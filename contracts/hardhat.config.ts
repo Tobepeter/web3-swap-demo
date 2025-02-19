c11dd981b37e2d6f1ae370ff043615dea367a133
tobe <583590744@qq.com>
2025-02-20 04:54:59 +0800

    增加合约地址自动化脚本

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,6 @@
-import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import dotenv from 'dotenv'
+import { HardhatUserConfig } from 'hardhat/config'
 
 dotenv.config()
 
@@ -25,16 +25,5 @@ const config: HardhatUserConfig = {
     apiKey: ETHERSCAN_API_KEY,
   },
 }
-
-// TODO: 有条件本地持有这些信息，方便前端使用
-
-/**
-合约部署完成！
--------------------
-部署地址汇总：
-MockERC20: 0x5FbDB2315678afecb367f032d93F642f64180aa3
-MockUSDC: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
-MockUniswapV2Pair: 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
- */
 
 export default config

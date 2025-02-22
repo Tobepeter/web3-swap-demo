f27aac564556f3b2def4a0d2d0c19ab1cdfed863
tobe <583590744@qq.com>
2025-02-23 04:21:35 +0800

    修复git构建typechain失败的问题

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,8 +3,11 @@ import '@nomicfoundation/hardhat-viem'
 
 import dotenv from 'dotenv'
 import { HardhatUserConfig } from 'hardhat/config'
+import { HttpNetworkUserConfig } from 'hardhat/types'
 
 dotenv.config()
+
+// TODO: 环境变量配置 ts intelisense
 
 // NOTE: sepolia private key only for test
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
@@ -17,12 +20,17 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
-  networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-    },
-  },
+  // NOTE: 其实 typechain 不需要 sepolia network 配置也可以工作
+  //  但是如果传递 有 undefined 内容的 sepolia 字段会导致报错
+  networks:
+    SEPOLIA_RPC_URL && SEPOLIA_PRIVATE_KEY
+      ? {
+          sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+          },
+        }
+      : {},
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
